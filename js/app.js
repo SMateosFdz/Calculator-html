b9fcb2e187b4cbe60ed210fa54cc59ccf145a0e1
Santiago Mateos <109747560+SMateosFdz@users.noreply.github.com>
2024-10-12 23:41:11 +0200

    HTML index file, css and js files

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function calcular(valor){
+    const forma = document.getElementById("forma");
+    let operandoA = forma["operandoA"];
+    let operandoB = forma["operandoB"];
+	let resultado;
+	
+	switch(valor){
+		case 1:
+		resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+		break;
+		
+		case 2:
+		resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+		break;
+		
+		case 3:
+		resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+		break;
+		
+		case 4:
+		resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+		break;
+		
+	}	
+
+    if(isNaN(resultado)){
+        document.getElementById("resultado").innerHTML = "<span>La operación no tiene números</span>";
+    }else{
+        document.getElementById("resultado").innerHTML = "<span>Resultado: " + resultado + "</span>";
+    }
+}
