212b1eb64627f4abd66b546f6669fe1b4b2bb048
sanmate <santiago.mateos@estudiantes.uva.es>
2025-01-16 17:28:44 +0100

    Update of the calculator, new operations added (remainder and sqrt), added a kind of informative popup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// Function to do the operations
+// It takes the values from the html and parses to Float
+// It also covers errors like the lack of an operand
 function calcular(valor){
     const forma = document.getElementById("forma");
     let operandoA = forma["operandoA"];
@@ -6,29 +9,65 @@ function calcular(valor){
 	
 	switch(valor){
 		case 1:
-		resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
-		break;
+			resultado = parseFloat(operandoA.value) + parseFloat(operandoB.value);
+			break;
 		
 		case 2:
-		resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
-		break;
+			resultado = parseFloat(operandoA.value) - parseFloat(operandoB.value);
+			break;
 		
 		case 3:
-		resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
-		break;
+			resultado = parseFloat(operandoA.value) * parseFloat(operandoB.value);
+			break;
 		
 		case 4:
-		resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
-		break;
+			resultado = parseFloat(operandoA.value) / parseFloat(operandoB.value);
+			break;
 
 		case 5:
-		resultado = parseInt(operandoA.value) ** parseInt(operandoB.value);
+			resultado = parseFloat(operandoA.value) % parseFloat(operandoB.value);
+			break;
+
+		case 6:
+			resultado = parseFloat(operandoA.value) ** parseFloat(operandoB.value);
+			break;
+
+		case 7:
+			if(operandoA.value != ""){ //necessary because Math.sqrt understands "" like 0
+				resultado = Math.sqrt(operandoA.value);
+			}else{
+				document.getElementById("resultado").innerHTML = "<span>Falta uno de los operandos</span>";
+			}
+			
 		
 	}	
 
     if(isNaN(resultado)){
-        document.getElementById("resultado").innerHTML = "<span>La operación no tiene números</span>";
+        document.getElementById("resultado").innerHTML = "<span>Falta uno de los operandos</span>";
     }else{
         document.getElementById("resultado").innerHTML = "<span>Resultado: " + resultado + "</span>";
     }
 }
+
+
+// Get the message, button and close button
+const modal = document.getElementById("infoModal");
+const btn = document.getElementById("infoButton");
+const closeButton = document.getElementsByClassName("close-button")[0];
+
+// When the user clicks the button, open the message 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks the cross, it hides the message
+closeButton.onclick = function() {
+    modal.style.display = "none";
+}
+
+// If the user clicks out of the message, it hides it
+/*window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}*/
