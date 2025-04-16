a6f6b02fcd213cd1423ef803108cc2c4a0769b93
snty181 <sanmafer18@gmail.com>
2025-04-16 22:04:25 +0200

    Feat: added 2 languages

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 // It takes the values from the html and parses to Float
 // It also covers errors like the lack of an operand
 function calcular(valor){
-    const forma = document.getElementById("forma");
-    let operandoA = forma["operandoA"];
-    let operandoB = forma["operandoB"];
+	const changeButton = document.getElementById("changeLanguage");
+    let operandoA = changeButton.innerHTML == "EN" ? formEs["operandoA"] : formEn["operandoA"];
+    let operandoB = changeButton.innerHTML == "EN" ? formEs["operandoB"] : formEn["operandoB"];
 	let resultado;
 	
 	switch(valor){
@@ -36,33 +36,66 @@ function calcular(valor){
 			if(operandoA.value != ""){ //necessary because Math.sqrt understands "" like 0
 				resultado = Math.sqrt(operandoA.value);
 			}else{
-				document.getElementById("resultado").innerHTML = "<span>Falta uno de los operandos</span>";
+				if(changeButton.innerHTML == "EN"){
+					document.getElementById("resultEs").innerHTML = "<span>Falta uno de los operandos</span>";
+				}else{
+					document.getElementById("resultEn").innerHTML = "<span>One of the operands is missing</span>";
+				}
 			}
-			
-		
 	}	
 
     if(isNaN(resultado)){
-        document.getElementById("resultado").innerHTML = "<span>Falta uno de los operandos</span>";
+		if(changeButton.innerHTML == "EN"){
+			document.getElementById("resultEs").innerHTML = "<span>Falta uno de los operandos</span>";
+		}else{
+			document.getElementById("resultEn").innerHTML = "<span>One of the operands is missing</span>";
+		}
     }else{
-        document.getElementById("resultado").innerHTML = "<span>Resultado: " + resultado + "</span>";
+		if(changeButton.innerHTML == "EN"){
+			document.getElementById("resultEs").innerHTML = "<span>Resultado: " + resultado + "</span>";
+		}else{
+			document.getElementById("resultEn").innerHTML = "<span>Result: " + resultado + "</span>";
+		} 
     }
 }
 
 
 // Get the message, button and close button
-const modal = document.getElementById("infoModal");
-const btn = document.getElementById("infoButton");
-const closeButton = document.getElementsByClassName("close-button")[0];
+const modalEs = document.getElementById("infoModalEs");
+const btnEs = document.getElementById("infoButtonEs");
+const closeButtonEs = document.getElementsByClassName("close-buttonEs")[0];
+
+const modalEn = document.getElementById("infoModalEn");
+const btnEn = document.getElementById("infoButtonEn");
+const closeButtonEn = document.getElementsByClassName("close-buttonEn")[0];
 
 // When the user clicks the button, open the message 
-btn.onclick = function() {
-    modal.style.display = "block";
+btnEs.onclick = function() {
+    modalEs.style.display = "block";
+	modalEs.style.position = "fixed";
+	modalEs.style.left = "50%";
+	modalEs.style.transform = "translate(-50%, 0)";
+	modalEs.style.zIndex = 2;
+
 }
 
 // When the user clicks the cross, it hides the message
-closeButton.onclick = function() {
-    modal.style.display = "none";
+closeButtonEs.onclick = function() {
+    modalEs.style.display = "none";
+}
+
+// When the user clicks the button, open the message 
+btnEn.onclick = function() {
+    modalEn.style.display = "block";
+	modalEn.style.position = "fixed";
+	modalEn.style.left = "50%";
+	modalEn.style.transform = "translate(-50%, 0)";
+	modalEn.style.zIndex = 2;
+}
+
+// When the user clicks the cross, it hides the message
+closeButtonEn.onclick = function() {
+    modalEn.style.display = "none";
 }
 
 // If the user clicks out of the message, it hides it
@@ -71,3 +104,24 @@ closeButton.onclick = function() {
         modal.style.display = "none";
     }
 }*/
+
+function changeLanguage(){
+	const changeButton = document.getElementById("changeLanguage");
+	changeButton.innerHTML == "ES" ? changeButton.innerHTML = "EN" : changeButton.innerHTML = "ES";
+	
+	if(changeButton.innerHTML == "EN"){
+		document.getElementById("es-language").style.display = "inline";
+		document.getElementById("en-language").style.display = "none";
+		document.getElementById("resultEs").innerHTML = "";
+		document.getElementById("resultEn").innerHTML = "";
+		formEs["operandoA"].value = "";
+		formEs["operandoB"].value = "";
+	}else{
+		document.getElementById("en-language").style.display = "inline";
+		document.getElementById("es-language").style.display = "none";
+		document.getElementById("resultEs").innerHTML = "";
+		document.getElementById("resultEn").innerHTML = "";
+		formEn["operandoA"].value = "";
+		formEn["operandoB"].value = "";
+	}
+}
